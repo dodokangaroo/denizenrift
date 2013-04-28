1c9f59c41ffb29b6f1650eb57355a5508ff415e7
josh <josh@tooty.(none)>
2013-04-28 16:02:58 +1000

    Build first time then start watching, wr won't execute straight away

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 killall node
+browserify -t coffeeify ./client/app.coffee > ./webserver/assets/js/app.js
 wr -v --exec "browserify -t coffeeify ./client/app.coffee > ./webserver/assets/js/app.js" client &
 coffee webserver/app.coffee &
